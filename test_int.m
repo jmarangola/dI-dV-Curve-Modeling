6e01ab486ae3f112383c0237241d6863622931bd
John <john.marangola@gmail.com>
2020-06-26 15:27:47 -0400

    updating

--- test_int.m
+++ test_int.m
@@ -1,6 +1,7 @@
 
 % first function
 %f = @(x,y) (x.^2-y.^2).*cos(x./(1+y.^2));
+format long
 g = @(x, A) A*((1)./(x.^3 + (x))).*((exp(-x) - 1)./(1 - exp(-x)));
 
 % Some limits of integration
@@ -8,7 +9,8 @@ lower_limit = 0;
 upper_limit = 3;
 
 % Define g as the integral of f(x,y) dy from a to b
-format long
+
 g = @(A) (integral(@(x) g(x,A) , -inf, inf))
 g(1)
+g(2)
 
