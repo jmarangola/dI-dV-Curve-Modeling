f47dbf2723e242578b799dd180cbf2bd2d181e10
John <john.marangola@gmail.com>
2020-06-26 15:24:00 -0400

    getting integral() to work on basic form of first integral with A as constant parameter to scale output

---
+++ test_int.m
@@ -0,0 +1,14 @@
+
+% first function
+%f = @(x,y) (x.^2-y.^2).*cos(x./(1+y.^2));
+g = @(x, A) A*((1)./(x.^3 + (x))).*((exp(-x) - 1)./(1 - exp(-x)));
+
+% Some limits of integration
+lower_limit = 0;
+upper_limit = 3;
+
+% Define g as the integral of f(x,y) dy from a to b
+format long
+g = @(A) (integral(@(x) g(x,A) , -inf, inf))
+g(1)
+
