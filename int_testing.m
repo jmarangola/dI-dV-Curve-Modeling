4ae6dd5a9b73e6c0631811d9c3c1e1c0b52b6315
John <john.marangola@gmail.com>
2020-06-27 16:28:25 -0400

    vpaintegral with approximate bounds returning numerical values for general function of P

--- int_testing.m
+++ int_testing.m
@@ -1,4 +1,5 @@
-% int function instead
+% vpaintegral docs:
+% https://www.mathworks.com/help/symbolic/vpaintegral.html
 syms A x y
 assume(x, 'real');
 assume(A, 'real');
@@ -9,10 +10,17 @@ g_gen = (((1)./(x.^3 + x)).* (((exp(-1.*i.*x.*A)) - 1)./(1 - exp(-1.*x))));
 % loop or matrix
 
 %Arange = -2000:1:2000
-g_part = subs(g_gen, A, 5) % arbitrary value for now trying to get it to work for matrix of values for A
+g_part = subs(g_gen, A, -0.3423) % arbitrary value for now trying to get it to work for matrix of values for A
 g = vpaintegral(g_part, x, 0.0001, inf) + y.*i
 
-P = vpaintegral(g, y, 0.0001, 200)
+format long
+P = vpaintegral(g, y, 0.0001, 2000000000000) + vpaintegral(g, y, -0.0001, -2000000000000) 
+% defining first integral from a->(b-delta) and second from (b+delta) to c where delta is arbitrarily small and b is a region of infinite discontinuity where the function is not differentiable
+% in the integral above, a and c are made arbitrarily large to approximate
+% right/left hand behavior as infinite bounds in vpa integral would make
+% later computations impossible by using the maximum possible number of function
+% calls in the stack frame
+% ie. error Failed precision goal. Try using 'MaxFunctionCalls
 
 
 
