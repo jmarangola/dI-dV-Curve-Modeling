144a0acbaed69b871e5cf732ab8f8301bd79cc77
John <john.marangola@gmail.com>
2020-06-27 16:06:46 -0400

    promising exact values generated using vpaintegral and piecewise defined integrals to avoid asymptotic regions

--- int_testing.m
+++ int_testing.m
@@ -1,15 +1,24 @@
 % int function instead
-syms A x
+syms A x y
 assume(x, 'real');
 assume(A, 'real');
 
-g = (((1)./(x.^3 + x)).*(((exp(-1.*i.*x.*A)) - 1)./(1 - exp(-1.*x))));
+g_gen = (((1)./(x.^3 + x)).* (((exp(-1.*i.*x.*A)) - 1)./(1 - exp(-1.*x))));
 % found a way to reliably and properly substitute variables in for symbolic
 % variables so that int function can work over a range of variables in a
 % loop or matrix
-g_alt = subs(g, A, 1)
 
-g_ = int(g_alt,x,0.001,inf)
+%Arange = -2000:1:2000
+g_part = subs(g_gen, A, 5) % arbitrary value for now trying to get it to work for matrix of values for A
+g = vpaintegral(g_part, x, 0.0001, inf) + y.*i
+
+P = vpaintegral(g, y, 0.0001, 200)
+
+
+
+
+
+
 
 
 
