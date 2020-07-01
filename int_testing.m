6f74b6715059b1e7cdc998ce3b2e87e2c8e32a1c
John <john.marangola@gmail.com>
2020-07-01 10:56:43 -0400

    plot of J(t) for j(t) integrated with respect to w over (inf, inf) for a range of t values. jplotting plots j(w) (underlying function of J(w, t) for a range of various t curves

--- int_testing.m
+++ int_testing.m
@@ -9,11 +9,6 @@ assume(y, 'real');
 % general form (using symbolic vars) 
 j_gen = (((1)./(w.^3 + w)).* (((exp(-1.*i.*w.*t)) - 1)./(1 - exp(-1.*w))));
 
-result = zeros(401);
-dom = -200:1:200;
-
-
-
 %for j=dom
 %    j_part = subs(j_gen, t, dom(n)); % substitute the nth element of dom into j_gen for symbolic var t
 %    result(n) = vpaintegral(j_part, w, 0.0001, inf);
@@ -23,21 +18,38 @@ dom = -200:1:200;
 
 %j_part = subs(j_gen, t, 0.0)
 %j = vpaintegral(j_part, w, 0.0001, inf)
-upperbound = 20000
-spacing = 25;
-iteration = 1;
-current_value = 0
-start =0.001
-j_part = subs(j_gen, t, -1000)
-while (iteration * spacing < upperbound)
-current_value = current_value + vpaintegral(j_part, w, start + spacing*(iteration-1), spacing*iteration + start);
+
+% a b c approach breaking up integral with a spacing constant (would be
+% nested for /nested while)
+%upperbound = 20000
+%spacing = 25;
+%iteration = 1;
+%current_value = 0
+%start =0.001
+%j_part = subs(j_gen, t, -1000)
+%while (iteration * spacing < upperbound)
+%current_value = current_value + vpaintegral(j_part, w, start + spacing*(iteration-1), spacing*iteration + start);
 %disp("lower bound");
 %disp(0.0001 + spacing*(iteration-1))
 %disp("upper bound")
 %disp(spacing*iteration + 0.0001)
-iteration = iteration+1;
+%iteration = iteration+1;
+%end
+allt = -100:.1:100
+output = []
+index = 1
+s = size(allt)
+while index <= s(2) % upper bound set by all t value vector
+    j_particular = subs(j_gen, t, allt(index));
+    inta = vpaintegral(j_particular, w, 0.0001, inf) ;
+    intb = vpaintegral(j_particular, w, -0.0001, -inf);
+    result = inta + intb;
+    output(index) = result;
+    index = index + 1
 end
-disp(current_value)
+
+
+
 
 
 % for first and second integral later
--- jplotting.m
+++ jplotting.m
@@ -1,21 +1,21 @@
 % plots what general form of j(t) NOT J(t) should look like for a variety
 % of t
 j_t = @(w, t) (((1)./(w.^3 + w)).* (((exp(-1.*1i.*w.*t)) - 1)./(1 - exp(-1.*w))));
-dom = -10:0.01:10;
-various_t_down = [-999999 -50000 -40000 -30000 -20000 -10000 -5000 -4000 -3000 -2000 -1000 -900 -800 -600 -500 -400 -100 -150 -90 -80 -70 -60 -50 -40 -30 -20 -10];
-s = size(various_t_down)
-various_t_up = -1.*flip(various_t_down);
-total_t = various_t_down + various_t_up;
+dom = -100:0.01:100;
+
+
+
+
+%total_t = various_t_down + various_t_up;
 
 
 % plots what general form of j(t) NOT J(t) should look like for a variety
 % of t
-j_t = @(w, t) (((1)./(w.^3 + w)).* (((exp(-1.*1i.*w.*t)) - 1)./(1 - exp(-1.*w))));
-dom = -10:0.01:10;
-various_t_down = [-999999 -50000 -40000 -30000 -20000 -10000 -5000 -4000 -3000 -2000 -1000 -900 -800 -600 -500 -400 -100 -150 -90 -80 -70 -60 -50 -40 -30 -20 -10];
-s = size(various_t_down)
-various_t_up = -1.*flip(various_t_down);
-total_t = various_t_down + various_t_up;
+%various_t_down = [-999999 -50000 -40000 -30000 -20000 -10000 -5000 -4000 -3000 -2000 -1000 -900 -800 -600 -500 -400 -100 -150 -90 -80 -70 -60 -50 -40 -30 -20 -10];
+%s = size(various_t_down)
+%various_t_up = -1.*flip(various_t_down);
+%total_t = various_t_down + various_t_up;
+total_t = linspace(-50000, 50000, 100)
 
 hold on
 for splot=total_t
