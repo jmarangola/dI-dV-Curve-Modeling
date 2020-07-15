6b7da9c639afe178ff76fed15ee89d396e4b709d
John <john.marangola@gmail.com>
2020-07-15 13:03:56 -0400

    formatting file

--- fullSumsERieman.m
+++ fullSumsERieman.m
@@ -3,6 +3,7 @@
 j = @(w, t) (((1)./(w.^3 + w)).* (((exp(-1.*1i.*w.*t)) - 1)./(1 - exp(-1.*w))));
 p = @(E, t) (exp(j(w, t) + 1i.*E.*t))
 
+% constants
 kb=1.381e-23;
 
 % differential step variables
@@ -12,22 +13,18 @@ dx_E = 0.1;
 
 % Bounds of integration
 tbounds1 = -20:dx_t:20;
-
-wbounds = 0.001:dx_w:20; % cannot do zero or there is infinite singularity div/==============i
+wbounds = 0.001:dx_w:20; % cannot do zero or there is infinite singularity div by 0
 
 % Domain/output must be dimensionally consistent for plot
 clear output
 domain = -200:1:200
 output = -200:1:200
 
-% Differential step
-
-
 % loop vars
 sum = 0;
 sum2 = 0;
 
-%iteration vars: matlab is 1 indexed not zero
+% ßiteration vars: matlab is 1 indexed not zero
 k = 1;
 for E=domain
     
