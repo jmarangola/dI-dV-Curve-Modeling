74c0cce138355d8830ddc205172e5b96bf3f62b3
John <john.marangola@gmail.com>
2020-07-16 17:15:42 -0400

    Working python numerical modeling for P(E)

--- Python/basic_nested.py
+++ Python/basic_nested.py
@@ -3,15 +3,32 @@ from cmath import exp
 import numpy as np
 import matplotlib as m
 import math
+import matplotlib.pyplot as plt
 
 x = 5
 y = 10
-w = 5
-t = 5
 
-Omega_ = [0.001, 20]
-t_ = [-200, 200]
-for x in range(0, 200):
-    outputs.append(((1)/(w**3 + w))* (( (exp(-1*1j*w*t)) - 1) / (1 - exp(-1*w)) ))
-    print(outputs[-1])
+
+dx = .1
+Omega_ = np.arange(0.001, 20, dx)
+t_ = np.arange(-20, 20, dx)
+E_ = np.arange(-200, 200, 1)
+outputs = []
+
+sum_ = 0
+sum_2 = 0
+for E in E_:
+    for t in t_:
+        for w in Omega_:
+            sum_ += ((((1)/(w**3 + w))* (( (exp(-1*1j*w*t)) - 1) / (1 - exp(-1*w)) ))*dx)
+        sum_2 += (exp(sum_ + 1j*E*t)*dx)
+        sum_ = 0
+    print(f"E: {E}")
+    outputs.append(sum_2)
+    sum_2 = 0
+    
+
+plt.plot(t_, outputs)
+plt.ylabel('P(E)')
+plt.show()
 
