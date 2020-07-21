1081d0862e8034a62c923e061938663c3859a01f
John <john.marangola@gmail.com>
2020-07-21 15:55:53 -0400

    adding constants class and parallel P(E) unfinished

---
+++ Constants.m
@@ -0,0 +1,15 @@
+classdef Constants
+    properties( Constant = true )
+        kb=1.381e-23; % K_b
+        hbar = 1.0545718e-34; % reduced planck constant 
+        h = 6.62607015e-34; % planck constant
+        e = 1.602e-19 % elementary charge
+        
+
+        % Physical Parameters
+        T = 7; % in K
+        C = 8e-18;
+        C_T = 0.84e-18;
+        R = 0.3
+    end
+ end
--- parallel_P.m
+++ parallel_P.m
@@ -1,14 +1,23 @@
-function parallel_P = parallel_E(V)
+function parallel_P() % no return value
     dx_E = 1; % this is currently not actually a differential x but a step variable but it will be later so for consistent convention it is defined dx_E
-    Ebounds % the plotdomain of P(E)
+    Ebounds = -200:dx_E:200; % the plotted domain of P(E)
+    output = -200:dx_E:200;
+    vec0 = 1:length(Ebounds); 
+    C = Constants;
+    for n=vec0
+        output(n) = parallel_j(Ebounds(n), C);
+        
+    end
     
+    figure
+    plot(Ebounds, output);
 end
 function out = parallel_j(E, C)
     dx_t = 0.1;
     tbounds = -20:dx_t:20;
     
     vec1 = 1:length(tbounds);
-    sum2 = 0
+    sum2 = 0;
   
     parfor x=vec1
         t = tbounds(x);
@@ -23,11 +32,13 @@ function result = function_testing(t)
     dx_w = 0.1;
     wbounds = 0.001:dx_w:20;
     vec2 = 1:length(wbounds);
-    j = @(w, t) (((1)./(w.^3 + w)) .* (((exp(-1.*1i.*w.*t)) - 1)./(1 - exp(-1.*w))));
+    ReZw = @(w) ( (C.R.^-1)./(C.R_k).*((w.^2.*(C_T + C).^2 ) + C.R.^(-2) ) );
+    j = @(w, t) -2 .* ReZw(w) .* (((1)./(w)).* (((exp(-1.*1i.*w.*t)) - 1)./(1 - exp((-1.*h.*w)./(kb*T)))));
+    
     parfor k=vec2
          w = wbounds(k);
          sum_ = sum_ + (j(w, t)*dx_w);
     end
     
-result = sum_
+result = sum_;
 end
