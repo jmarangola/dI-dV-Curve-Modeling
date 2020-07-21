4d7c2d6474d4ecbc38f3e256571d64948932900b
John <john.marangola@gmail.com>
2020-07-21 14:54:16 -0400

    start of parallel_j with matlab function nested parfor

--- parallel_j.m
+++ parallel_j.m
@@ -1,25 +1,22 @@
 
-function result = parallel_j(tbounds)
-
-    if exist('x','var')
-        j = @(w, t) (((1)./(w.^3 + w)).* (((exp(-1.*1i.*w.*t)) - 1)./(1 - exp(-1.*w))));
-        dx_w = 0.1;
-        dx_t = 0.1;
-    end
+function result = parallel_j(p)
+    dx_w = 0.1;
+    dx_t = 0.1;
+    tbounds = -50:dx_t:50
+     j = @(w, t) (((1)./(w.^3 + w)).* (((exp(-1.*1i.*w.*t)) - 1)./(1 - exp(-1.*w))));
+   
     
-    vec = 1:1:length(tbounds); % parfor can only iterate through consecutive elements
-    sum = 0;
+    vec = 1:length(tbounds); % parfor can only iterate through consecutive elements
+    sum2 = 0;
     
     parfor i=vec % single step index iteration vector for parfor
-        sum = integrate_omega(0.01:1:20)
-        
+        sum = integrate_omega(0.01:1:20, tbounds(i)) % i th element of vtbounds is current t
         sum2 = sum2 + (1/(2*pi*hbar))*(exp(sum + 1i.*E.*t/hbar))*dx_t;
-        sum = 0;
     end
     
-    result = sum
+    result = sum2
 
-function  sub = integrate_omega(wbounds)
+function  sub = integrate_omega(wbounds, t)
 
     sum_ = 0; % scope issue -> subfunction must be sum_
     parfor w=wbounds
