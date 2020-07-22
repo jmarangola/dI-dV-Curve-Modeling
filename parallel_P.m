1900f40b01f7e3ed8f3bb7334c1719b6a5c01335
John <john.marangola@gmail.com>
2020-07-21 21:00:24 -0400

    Working parallel processing to generate data much faster using parallel computing toolbox

--- parallel_P.m
+++ parallel_P.m
@@ -1,44 +1,65 @@
 function parallel_P() % no return value
-    dx_E = 1; % this is currently not actually a differential x but a step variable but it will be later so for consistent convention it is defined dx_E
-    Ebounds = -200:dx_E:200; % the plotted domain of P(E)
-    output = -200:dx_E:200;
-    vec0 = 1:length(Ebounds); 
-    C = Constants;
+    kb=1.381e-23; % K_b
+    hbar = 1.0545718e-34; % reduced planck constant 
+    h = 6.62607015e-34; % planck constant
+    e_ = 1.602e-19; % elementary charge
+    R_k = h/(e_^2); % R_k
+
+
+    % Physical Parameters
+    T = 7; % in K
+    C = 8e-18;
+    C_T = 0.84e-18;
+    R = 0.3*R_k;
+    Ebounds = -200:1:200;
+    vec0 = 1:length(Ebounds)
+
     for n=vec0
-        output(n) = parallel_j(Ebounds(n), C);
-        
+        output(n) = parallel_j(Ebounds(n));
+        disp(n);
     end
     
     figure
     plot(Ebounds, output);
-end
-function out = parallel_j(E, C)
-    dx_t = 0.1;
+
+function out = parallel_j(E)
+    hbar = 1.0545718e-34; % reduced planck constant 
+    h = 6.62607015e-34; % planck constant
+    e_ = 1.602e-19; % elementary charge
+    dx_t = .01;
     tbounds = -20:dx_t:20;
-    
     vec1 = 1:length(tbounds);
     sum2 = 0;
   
     parfor x=vec1
         t = tbounds(x);
         omega = function_testing(t);
-        sum2 = sum2 + (1/(2*pi*C.hbar))*(exp(omega + 1i.*E.*t/C.hbar))*dx_t;
+        sum2 = sum2 + (1/(2*pi*hbar))*(exp(omega + 1i.*E.*t/hbar))*dx_t;
     end
     out = sum2;
-end
+
 
 function result = function_testing(t)
-    sum_ = 0; 
+    kb=1.381e-23; % K_b
+    hbar = 1.0545718e-34; % reduced planck constant 
+    h = 6.62607015e-34; % planck constant
+    e_ = 1.602e-19;
+    R_k = h/(e_^2); % R_k
+
+    % Physical Parameters
+    T = 7; % in K
+    C = 8e-18;
+    C_T = 0.84e-18;
+    R = 0.3*R_k;
     dx_w = 0.1;
     wbounds = 0.001:dx_w:20;
     vec2 = 1:length(wbounds);
-    ReZw = @(w) ( (C.R.^-1)./(C.R_k).*((w.^2.*(C_T + C).^2 ) + C.R.^(-2) ) );
+    ReZw = @(w) ( (R.^-1)./(R_k).*((w.^2.*(C_T + C).^2 ) + R.^(-2) ) );
     j = @(w, t) -2 .* ReZw(w) .* (((1)./(w)).* (((exp(-1.*1i.*w.*t)) - 1)./(1 - exp((-1.*h.*w)./(kb*T)))));
-    
-    parfor k=vec2
+    sum_ = 0;
+    for k=vec2
          w = wbounds(k);
          sum_ = sum_ + (j(w, t)*dx_w);
     end
     
 result = sum_;
-end
